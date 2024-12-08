c8c8f2018193492bc7d67dc146b15b3b14e7f915
ValkinDED <sonyerik289@gmail.com>
2024-12-08 10:17:31 +0100

    goit-js-hw-06

--- js/task-3.js
+++ js/task-3.js
@@ -1,42 +1,34 @@
-const sortByDescendingFriendCount = users =>
-  users.toSorted((a, b) => b.friends.length - a.friends.length);
+class StringBuilder {
+  #value; // Оголошення приватної властивості
 
-console.log(
-  sortByDescendingFriendCount([
-    {
-      name: 'Moore Hensley',
-      friends: ['Sharron Pace'],
-      gender: 'male',
-    },
-    {
-      name: 'Sharlene Bush',
-      friends: ['Briana Decker', 'Sharron Pace'],
-      gender: 'female',
-    },
-    {
-      name: 'Ross Vazquez',
-      friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-      gender: 'male',
-    },
-    {
-      name: 'Elma Head',
-      friends: ['Goldie Gentry', 'Aisha Tran'],
-      gender: 'female',
-    },
-    {
-      name: 'Carey Barr',
-      friends: ['Jordan Sampson', 'Eddie Strong'],
-      gender: 'male',
-    },
-    {
-      name: 'Blackburn Dotson',
-      friends: ['Jacklyn Lucas', 'Linda Chapman'],
-      gender: 'male',
-    },
-    {
-      name: 'Sheree Anthony',
-      friends: ['Goldie Gentry', 'Briana Decker'],
-      gender: 'female',
-    },
-  ])
-);
+  constructor(initialValue) {
+    this.#value = initialValue; // Ініціалізація приватної властивості
+  }
+
+  getValue() {
+    return this.#value; // Повернення значення приватної властивості
+  }
+
+  padEnd(str) {
+    this.#value += str; // Додавання рядка в кінець значення
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value; // Додавання рядка на початок значення
+  }
+
+  padBoth(str) {
+    this.padStart(str); // Виклик padStart
+    this.padEnd(str); // Виклик padEnd
+  }
+}
+
+// Код для перевірки
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
